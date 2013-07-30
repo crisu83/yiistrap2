8d3cacf5044f8dac6226e786dd81aaaa8a2c5953
Christoffer Niska <ChristofferNiska@gmail.com>
2013-07-30 15:22:37 +0300

    Update ArrayHelper.php and add tests for it

--- tests/unit/ArrayHelperTest.php
+++ tests/unit/ArrayHelperTest.php
@@ -1,6 +1,8 @@
 <?php
 use Codeception\Util\Stub;
 
+use yiistrap\helpers\ArrayHelper;
+
 class ArrayHelperTest extends \Codeception\TestCase\Test
 {
    /**
@@ -16,10 +18,60 @@ class ArrayHelperTest extends \Codeception\TestCase\Test
     {
     }
 
-    // tests
-    public function testMe()
+    public function testPopValue()
     {
-
+        $array = array('key' => 'value');
+        $this->assertEquals('value', ArrayHelper::popValue($array, 'key'));
+        $this->assertArrayNotHasKey('key', $array);
     }
 
+    public function testDefaultValue()
+    {
+        $array = array();
+        ArrayHelper::defaultValue($array, 'key', 'default');
+        $this->assertEquals('default', ArrayHelper::getValue($array, 'key'));
+        ArrayHelper::defaultValue($array, 'key', 'value');
+        $this->assertEquals('default', ArrayHelper::getValue($array, 'key'));
+    }
+
+    public function testDefaultValues()
+    {
+        $array = array('my' => 'value');
+        ArrayHelper::defaultValues($array, array('these' => 'are', 'my' => 'defaults'));
+        $this->assertEquals('are', ArrayHelper::getValue($array, 'these'));
+        $this->assertEquals('value', ArrayHelper::getValue($array, 'my'));
+    }
+
+    public function testRemoveValues()
+    {
+        $array = array('these' => 'are', 'my' => 'values');
+        ArrayHelper::removeValues($array, array('these', 'my'));
+        $this->assertArrayNotHasKey('these', $array);
+        $this->assertArrayNotHasKey('my', $array);
+    }
+
+    public function testCopyValues()
+    {
+        $a = array('key' => 'value');
+        $b = array();
+        $array = ArrayHelper::copyValues(array('key'), $a, $b);
+        $this->assertEquals($a, $array);
+        $a = array('key' => 'value');
+        $b = array('key' => 'other');
+        $array = ArrayHelper::copyValues(array('key'), $a, $b, true);
+        $this->assertEquals($a, $array);
+    }
+
+    public function testMoveValues()
+    {
+        $a = array('key' => 'value');
+        $b = array();
+        $array = ArrayHelper::moveValues(array('key'), $a, $b);
+        $this->assertArrayNotHasKey('key', $a);
+        $this->assertEquals('value', ArrayHelper::getValue($array, 'key'));
+        $a = array('key' => 'value');
+        $b = array('key' => 'other');
+        $array = ArrayHelper::moveValues(array('key'), $a, $b, true);
+        $this->assertEquals('value', ArrayHelper::getValue($array, 'key'));
+    }
 }
--- yiistrap/helpers/ArrayHelper.php
+++ yiistrap/helpers/ArrayHelper.php
@@ -16,5 +16,106 @@ namespace yiistrap\helpers;
  */
 class ArrayHelper extends \yii\helpers\ArrayHelperBase
 {
+    /**
+     * Removes and returns a specific value from the given array (or the default value if not set).
+     * @param mixed $array the array to pop the item from.
+     * @param string $key the item key.
+     * @param mixed $default the default value.
+     * @return mixed the value.
+     */
+    public static function popValue(&$array, $key, $default = null)
+    {
+        $value = self::getValue($array, $key, $default);
+        if (is_array($array)) {
+            unset($array[$key]);
+        } else {
+            unset($array->$key);
+        }
+        return $value;
+    }
 
+    /**
+     * Sets the default value for a specific key in the given array.
+     * @param mixed $array the array to set value for.
+     * @param string $key the item key.
+     * @param mixed $value the default value.
+     */
+    public static function defaultValue(&$array, $key, $value)
+    {
+        if (is_array($array)) {
+            if (!isset($array[$key])) {
+                $array[$key] = $value;
+            }
+        } else {
+            if (!isset($array->$key)) {
+                $array->$key = $value;
+            }
+        }
+    }
+
+    /**
+     * Sets a set of default values for the given array.
+     * @param mixed $array the array to set values for.
+     * @param array $values the default values.
+     */
+    public static function defaultValues(&$array, array $values)
+    {
+        foreach ($values as $name => $value) {
+            self::defaultValue($array, $name, $value);
+        }
+    }
+
+    /**
+     * Removes a set of items from the given array.
+     * @param array $array the array to remove from.
+     * @param array $keys the keys to remove.
+     */
+    public static function removeValues(array &$array, array $keys)
+    {
+        foreach ($keys as $key) {
+            self::remove($array, $key);
+        }
+    }
+
+    /**
+     * Copies the given values from one array to another.
+     * @param array $keys the keys to copy.
+     * @param array $from the array to copy from.
+     * @param array $to the array to copy to.
+     * @param boolean $force whether to allow overriding of existing values.
+     * @return array the options.
+     */
+    public static function copyValues(array $keys, array $from, array $to, $force = false)
+    {
+        foreach ($keys as $key) {
+            if (isset($from[$key])) {
+                if ($force || !isset($to[$key])) {
+                    $to[$key] = self::getValue($from, $key);
+                }
+            }
+        }
+        return $to;
+    }
+
+    /**
+     * Moves the given values from one array to another.
+     * @param array $keys the keys to move.
+     * @param array $from the array to move from.
+     * @param array $to the array to move to.
+     * @param boolean $force whether to allow overriding of existing values.
+     * @return array the options.
+     */
+    public static function moveValues(array $keys, array &$from, array $to, $force = false)
+    {
+        foreach ($keys as $key) {
+            if (isset($from[$key])) {
+                $value = self::popValue($from, $key);
+                if ($force || !isset($to[$key])) {
+                    $to[$key] = $value;
+                    self::remove($from, $key);
+                }
+            }
+        }
+        return $to;
+    }
 }
